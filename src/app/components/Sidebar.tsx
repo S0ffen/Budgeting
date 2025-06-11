5791901d8d23b726a16b23be36e5afc2c97e389c
skierka <Twarogowy.potwor@gmail.com>
2025-06-11 23:11:01 +0200

    ./.....

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -18,7 +18,7 @@ const users = [
 export default function Sidebar({ onSelectUser }: SidebarProps) {
   return (
     <aside className="w-60 h-full bg-white border-r border-black p-4 overflow-y-auto">
-      <h2 className="text-xl font-bold mb-4 border-b pb-2">Users</h2>
+      <h2 className="text-xl font-bold mb-4 border-b pb-2">Users3</h2>
       <ul className="space-y-2">
         {users.map((user, idx) => (
           <li
