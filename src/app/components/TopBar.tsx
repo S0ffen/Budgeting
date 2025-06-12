5aa3438b45baa2cb6d009fc5ee7122803c787ed5
S0ffen <whitevolf314@gmail.com>
2025-06-12 23:40:52 +0200

    added shadcn

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -1,8 +1,13 @@
 // src/app/components/InputBar.tsx
+import { Button } from "@/components/ui/button";
+
 export default function TopBar() {
   return (
     <div className="h-14 w-full bg-blue-400  border-b border-black p-4">
       <p className="font-semibold">LIST NAME</p>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
